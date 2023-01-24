9db3e82e831e5c912c215af2b2c50674255c0c48
ryan <ryanww86@gmail.com>
2023-01-24 17:00:09 -0500

    add pythonanywhere to the allowed_hosts in setting file

--- CarBlog/easyBakery/settings.py
+++ CarBlog/easyBakery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%ov0!-+@ep*6swauhm@@zfd8ts-ft&6+@)c5^q69-xxzte763y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
